fc242a184c782d6f229f166591abbde2f2b15739
Nick Driscoll <nick.a.mavros@gmail.com>
2020-02-16 06:51:06 -0500

    Floor and wall

--- shaders/mapped_vertex.glsl
+++ shaders/mapped_vertex.glsl
@@ -1,7 +1,6 @@
 #version 330 core
 
 in vec3 position;
-in vec3 normal;
 in vec2 uv;
 out vec4 f_normal;
 out vec2 f_uvs;
@@ -11,11 +10,5 @@ uniform mat4 model_matrix;
 
 void main() {
     f_uvs = uv;
-    
-    //Send world space representation of normal vector
-	//We use a normal matrix instead of just the model matrix so that non-uniform scaling doesn't mess up the normal vector
-	mat4 normal_matrix = transpose(mat4(inverse(mat3(model_matrix))));
-	f_normal = normal_matrix * vec4(normal, 0.0);
-
     gl_Position = mvp * vec4(position, 1.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 extern crate nalgebra_glm as glm;
 use std::ptr;
+use std::time::{Instant, SystemTime, UNIX_EPOCH};
 use glfw::{Context, WindowEvent, WindowMode};
 use gl::types::*;
-use ozy_engine::{glutil, init};
+use ozy_engine::{glutil, init, routines};
 
 fn main() {
 	let window_size = (1920, 1080);
+	let aspect_ratio = window_size.0 as f32 / window_size.1 as f32;
 	let (mut glfw, mut window, events) = init::glfw_window(window_size, WindowMode::Windowed, 3, 3, "Whee Tanks");
+
+	//Make the window non-resizable
+	window.set_resizable(false);
 
 	//Configure what kinds of events GLFW will listen for
 	window.set_key_polling(true);
@@ -27,63 +32,146 @@ fn main() {
 		gl::ClearColor(0.53, 0.81, 0.92, 1.0);							//Set clear color. A pleasant blue
 	}
 
-	//Compile shaders
+	//Compile shader
 	let mapped_shader = unsafe { glutil::compile_program_from_files("shaders/mapped_vertex.glsl", "shaders/mapped_fragment.glsl") };
 
 	//Define the floor plane
-	let (floor_vao, floor_texture, floor_matrix) = unsafe {
+	let arena_ratio = 16.0 / 9.0;
+	let (floor_vao, floor_texture, floor_matrix, floor_count) = unsafe {
+		let tex_scale = 2.0;
 		let vertices = [
-			//Positions				Normals						Texture coordinates
-			-0.5, 0.0, -0.5,		0.0, 1.0, 0.0,				0.0, 0.0,
-			0.5, 0.0, -0.5,			0.0, 1.0, 0.0,				1.0, 0.0,
-			-0.5, 0.0, 0.5,			0.0, 1.0, 0.0,				0.0, 1.0,
-			0.5, 0.0, 0.5,			0.0, 1.0, 0.0,				1.0, 1.0
+			//Positions				Texture coordinates
+			-0.5, 0.0, -0.5,		0.0, 0.0,
+			0.5, 0.0, -0.5,			tex_scale, 0.0,
+			-0.5, 0.0, 0.5,			0.0, tex_scale,
+			0.5, 0.0, 0.5,			tex_scale, tex_scale
 		];
 		let indices = [
 			0u16, 1, 2,
 			3, 2, 1
 		];
 		let tex_params = [
-			(gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE),
-			(gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE),
+			(gl::TEXTURE_WRAP_S, gl::REPEAT),
+			(gl::TEXTURE_WRAP_T, gl::REPEAT),
 			(gl::TEXTURE_MIN_FILTER, gl::LINEAR),
 			(gl::TEXTURE_MAG_FILTER, gl::LINEAR)
 		];
-		let matrix: glm::TMat4<f32> = glm::identity();
+		let matrix = glm::scaling(&glm::vec3(9.0*arena_ratio, 10.0, 10.0));
 
 		(
-			glutil::create_vertex_array_object(&vertices, &indices, &[3, 3, 2]),
-			glutil::load_texture("textures/checkerboard.jpg", &tex_params),
-			matrix
+			glutil::create_vertex_array_object(&vertices, &indices, &[3, 2]),
+			glutil::load_texture("textures/wood_veneer/albedo.png", &tex_params),
+			matrix,
+			indices.len() as GLsizei
 		)
 	};
+
+	//Define upper wall
+	let (wall_vao, wall_texture, mut wall_matrix, wall_count) = unsafe {
+		let vertices = [
+			//Positions				Texture Coordinates
+			-0.5, -0.5, -0.5,		0.0, 0.0,
+			0.5, -0.5, -0.5,		1.0, 0.0,
+			-0.5, 0.5, -0.5,		0.0, -1.0,
+			0.5, 0.5, -0.5,			2.0, 0.0,
+			-0.5, -0.5, 0.5,		0.0, 1.0,
+			-0.5, 0.5, 0.5,			0.0, 2.0,
+			0.5, -0.5, 0.5,			1.0, 1.0,
+			0.5, 0.5, 0.5,			2.0, 1.0
+		];
+		let indices = [
+			//Front
+			0u16, 1, 2,
+			3, 2, 1,
+			
+			//Left
+			0, 2, 4,
+			2, 5, 4,
+
+			//Right
+			3, 1, 6,
+			7, 3, 6,
+
+			//Back
+			5, 7, 4,
+			7, 6, 4,
+
+			//Bottom
+			4, 1, 0,
+			4, 6, 1,
+			
+			//Top
+			7, 5, 2,
+			7, 2, 3
+		];
+		let vao = glutil::create_vertex_array_object(&vertices, &indices, &[3, 2]);
+
+		let tex_params = [
+			(gl::TEXTURE_WRAP_S, gl::REPEAT),
+			(gl::TEXTURE_WRAP_T, gl::REPEAT),
+			(gl::TEXTURE_MIN_FILTER, gl::LINEAR),
+			(gl::TEXTURE_MAG_FILTER, gl::LINEAR)
+		];
+		let albedo = glutil::load_texture("textures/steel_plate/albedo.png", &tex_params);
+		let matrix = glm::translation(&glm::vec3(0.0, 0.5, 5.5)) * glm::scaling(&glm::vec3(9.0*arena_ratio, 1.0, 1.0));
+		(vao, albedo, matrix, indices.len() as GLsizei)
+	};
+
+	//The view-projection matrix is constant
+	let view_matrix = glm::look_at(&glm::vec3(0.0, 1.5, -1.0), &glm::vec3(0.0, 0.0, 0.0), &glm::vec3(0.0, 1.0, 0.0));
+	let ortho_size = 5.0;
+	let projection_matrix = glm::ortho(-ortho_size*aspect_ratio, ortho_size*aspect_ratio, -ortho_size, ortho_size, -ortho_size, ortho_size);
+
+	let mut last_frame_instant = Instant::now();
+	let mut elapsed_time = 0.0;
 	
 	//Main loop
     while !window.should_close() {
+		//Calculate time since the last frame started in seconds
+		let time_delta = {
+			let frame_instant = Instant::now();
+			let dur = frame_instant.duration_since(last_frame_instant);
+			last_frame_instant = frame_instant;
+
+			//There's an underlying assumption here that frames will always take less than one second to complete
+			(dur.subsec_millis() as f32 / 1000.0) + (dur.subsec_micros() as f32 / 1_000_000.0)
+		};
+		elapsed_time += time_delta;
+
+		//Handle window events
         for (_, event) in glfw::flush_messages(&events) {
             match event {
                 WindowEvent::Close => { window.set_should_close(true); }
                 _ => {}
             }
         }
-
-		let view_matrix = glm::look_at(&glm::vec3(0.0, 1.0, -1.0), &glm::vec3(0.0, 0.0, 0.0), &glm::vec3(0.0, 1.0, 0.0));
-		let ortho_size = 3.0;
-		let projection_matrix = glm::ortho(-ortho_size, ortho_size, -ortho_size, ortho_size, -ortho_size, ortho_size);
 		
+		//-----------Simulating-----------
+		//wall_matrix = glm::translation(&glm::vec3(f32::sin(elapsed_time*2.0)*4.0, 1.0, f32::sin(elapsed_time))) * routines::uniform_scale(0.5);
+
 		//-----------Rendering-----------
 		unsafe {
 			gl::Viewport(0, 0, window_size.0 as GLsizei, window_size.1 as GLsizei);
 			gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
 
-			//Render floor plane
+			//Bind the GLSL program
 			gl::UseProgram(mapped_shader);
+
+			//Render floor plane
 			glutil::bind_matrix4(mapped_shader, "mvp", &(projection_matrix * view_matrix * floor_matrix));
 			glutil::bind_matrix4(mapped_shader, "model_matrix", &floor_matrix);
 			gl::ActiveTexture(gl::TEXTURE0);
 			gl::BindTexture(gl::TEXTURE_2D, floor_texture);
 			gl::BindVertexArray(floor_vao);
-			gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_SHORT, ptr::null());
+			gl::DrawElements(gl::TRIANGLES, floor_count, gl::UNSIGNED_SHORT, ptr::null());
+
+			//Render upper wall
+			glutil::bind_matrix4(mapped_shader, "mvp", &(projection_matrix * view_matrix * wall_matrix));
+			glutil::bind_matrix4(mapped_shader, "model_matrix", &wall_matrix);
+			gl::ActiveTexture(gl::TEXTURE0);
+			gl::BindTexture(gl::TEXTURE_2D, wall_texture);
+			gl::BindVertexArray(wall_vao);
+			gl::DrawElements(gl::TRIANGLES, wall_count, gl::UNSIGNED_SHORT, ptr::null());
 		}
 
 		window.render_context().swap_buffers();
